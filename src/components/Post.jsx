2b5e3da7d6df072549d2596dabb46fc91bcc6966
eunha <dmsgk2323@naver.com>
2025-04-07 16:35:09 +0900

    main페이지 디테일추가

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './Post.module.css';
 import fireIcon from '../assets/fireicon.svg';
-const Post = ({ children, onClick }) => {
+
+const Post = () => {
   return (
-    <li>
-        <div className={styles.titleWrapper} >
-            <h3>주 3회 이상 운동하기 습관 챌린지</h3>
-            <img  className={styles.fireIcon } src={fireIcon} alt="인기 아이콘" width={44} height={44} />
-            <div className={styles.popularIcon}>인기스터디</div>
-        </div>
-        <p>헬스 ㅇ 러닝 ㅇ 운동종목 상관 ㄴㄴ</p>
-        <div className={styles.tag}>손캠형</div>
-    </li>
+    <div className={styles.card}>
+      <div className={styles.titleWrapper}>
+        <h3>주 3회 이상 운동하기 습관 챌린지</h3>
+        <img className={styles.fireIcon} src={fireIcon} alt="인기 아이콘" width={44} height={44} />
+        <div className={styles.popularIcon}>인기스터디</div>
+      </div>
+      <p className={styles.des}>헬스 ㅇ 러닝 ㅇ 운동종목 상관 ㄴㄴ</p>
+      <div className={styles.tag}>손캠형</div>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,29 @@ import styles from './Home.module.css';
 import Banner from '../components/Banner';
 const Home = ({ children, onClick }) => {
 
-    const studyList = Array(10).fill(0).map((_, i) => i + 1); // [1, 2, ..., 10] 
+  const studyList = Array(10).fill(0).map((_, i) => i + 1); // [1, 2, ..., 10] 
   return (
     <div className={styles.wrapper}>
 
-    <Banner/>
-    <section>  
-        <h2 className={styles.title}>모든 스터디 모아보기</h2>
-        <ul>
-        {studyList.map((num) => (
-            <Post className={styles.list} key={num} />
-      ))}
+      <Banner />
+      <section>
+        <div className={styles.navWrapper}>
+          <h2 className={styles.title}>모든 스터디 모아보기</h2>
+          <ul>
+            <li>최신순</li>
+            <li>인기순</li>
+            <li>마감안된순</li>
+          </ul>
+        </div>
+        <ul className={styles.listWrapper}>
+          {studyList.map((num) => (
+            <li className={styles.list} key={num}>
+              <Post />
+            </li>
+          ))}
         </ul>
-    </section>
+
+      </section>
     </div>
   );
 };
