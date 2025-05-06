25b4f6ff5db1c1011a4241d94a1fdab60267f9c6
eunha <dmsgk2323@naver.com>
2025-05-06 15:58:23 +0900

    배너 이미지 변경

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Banner.module.css';
-import ad from '../assets/ad.svg';
-const Banner = ({ children, onClick }) => {
+import ad from '../assets/ad.png';
+
+const Banner = ({ onClick }) => {
   return (
      <article>
-     
-        <img  className={styles.fireIcon} src={ad} alt="광고배너" />
+        <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" onClick={onClick}>
+          <img className={styles.fireIcon} src={ad} alt="광고배너" />
+        </a>
         <div className={styles.buttonWrapper}>
             <p className={styles.question}>내가 찾는 스터디가 없다면?</p>
             <a href="https://walla.my/survey/ikykdUun43CzkEyIQ7gt" target="_blank" rel="noopener noreferrer">
